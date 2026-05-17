53bb28da4a6d7be6c8dac47b53738eaca138f8df
Arssa02 <arsenijedjordjevic2002@gmail.com>
2026-05-17 10:39:43 +0200

    dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,11 @@
 import { useState } from 'react'
-
+import { Dashboard } from './Dashboard'
 function App() {
-  const [time, setTime] = useState('')
-
-  const handleSubmit = async (e) => {
-    e.preventDefault() 
-
-    if (!time) {
-      alert('Please select a date and time first!')
-      return
-    }
-
-    try {
-      const response = await fetch('https://your-api-endpoint.com/api/time', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ scheduledTime: time }),
-      })
-
-      if (response.ok) {
-        alert('Time sent successfully!')
-      } else {
-        alert('Failed to send time to the server.')
-      }
-    } catch (error) {
-      console.error('Error sending data:', error)
-      alert('A network error occurred.')
-    }
-  }
-
+  
   return (
-    <>
-      <div className=''>
-        <form onSubmit={handleSubmit}>
-          <h1>Check water visibility predictions</h1>
-          <input 
-            type="datetime-local" 
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-          /> 
-          <button type="submit">Check</button>        
-        </form>
-      </div>
-    </>
+    <div>
+      <Dashboard />
+    </div>
   )
 }
 
---
+++ frontend/src/Dashboard.jsx
@@ -0,0 +1,3 @@
+export function Dashboard() {
+  return <iframe src="./public/water_clarity_dashboard.html" style={{ width: "100vw", height: "100vh", border: "none" }} title="My page" />;
+}
